99360941f517c92bbce4e19c36a9c17df34a50b0
Nick Power <powen2@gmail.com>
2026-03-27 09:46:40 +0200

    Added view for sendconfirmation

--- backend/gbsapp/views.py
+++ backend/gbsapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from services import EmailService
+from .services import EmailService
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
