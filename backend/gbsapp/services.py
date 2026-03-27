2627fea1a3aab543ae93c4366cf3bb038e1c13a2
Nick Power <powen2@gmail.com>
2026-03-27 09:27:47 +0200

    Testing a view

---
+++ backend/gbsapp/services.py
@@ -0,0 +1,8 @@
+class EmailService:
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def sendWelcomeEmail(name,email):
+        print("Email Sent")
+        pass
--- backend/gbsapp/views.py
+++ backend/gbsapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from services import EmailService
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+class sendConfirmEmail(APIView):
+    def get(self, request):
+        EmailService.sendWelcomeEmail('test','email@email.com')
+        return Response({'message': 'Welcome email sent'}, status=status.HTTP_200_OK)
