6d82893c7bb2f144427a3a98fa107f4452ea438e
Nick Power <powen2@gmail.com>
2026-04-09 15:07:12 +0300

    Edited models script based on Esko's script. Made migrations and updated database.

--- backend/gbsapp/views.py
+++ backend/gbsapp/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-
+#from rest_framework.views import APIView
+#from rest_framework.response import Response
+#from rest_framework import status
+from gbsapp.models import BillingCase
 # Create your views here.
 #class sendConfirmEmail(APIView):
    # def get(self, request):
@@ -10,5 +10,14 @@ from rest_framework import status
    #     
    #     return Response({'message': 'Welcome email sent'}, status=status.HTTP_200_OK)
  
+
 def laskutus(request):
-    return render(request, 'gbsapp/laskutus.html')
+    invoices = BillingCase.objects.all()
+    return render(request, 'gbsapp/laskutus/laskutus.html', {'invoices': invoices})
+
+def lasku_new(request):
+    return render(request, 'gbsapp/laskutus/lasku_new.html')
+
+def lasku_detail(request, pk):
+    invoice = BillingCase.objects.get(pk=pk)
+    return render(request, 'gbsapp/laskutus/lasku_detail.html', {'invoice': invoice})
