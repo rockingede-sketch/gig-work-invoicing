8e00dcb708dddd7f3cead8ecf2ad9ba9a75e04f1
Nick Power <powen2@gmail.com>
2026-04-21 09:07:47 +0300

    Merging local

--- backend/gbsapp/views.py
+++ backend/gbsapp/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render
 from gbsapp.models import BillingCase
+from gbsapp.models import Invoice
 from django.db.models import Sum, Q
 from django.utils import timezone
+from datetime import datetime
 from datetime import timedelta
 from .forms import BillingCaseForm
 from django.shortcuts import redirect
 from .services.services import BillingCalculators
 from django.http import HttpResponse
+from .services import make_invoice
+import os
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "invoice_log.log"),
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filemode='a')
 
 def laskutus(request):
 
@@ -92,3 +103,66 @@ def group_billing_fields(request):
 def e_invoice_address(request):
     form = BillingCaseForm()
     return render(request,'gbsapp/form_sections/einvoice_address.html',{'form':form})
+
+## pdf-laskun tekeminen ja tallentaminen tietokantaan:
+def make_pdf_invoice(billing_case_id: int):
+    try:
+        inv_case = BillingCase.objects.get(id=billing_case_id)
+        # haetaan laskutusasiakkaan tiedot:
+        billing_cust = inv_case.billing_cust_id
+        p_name = billing_cust.company_name
+        p_address = billing_cust.address
+        p_zip = billing_cust.postcode
+        p_city = billing_cust.postoffice
+        p_country = ''
+        p_business_id = billing_cust.company_id
+        i_service = inv_case.job_date.strftime("%Y-%m-%d") + ", " + inv_case.work_task + ", " + inv_case.work_description + ", " + inv_case.job_location
+        i_pcs = inv_case.number_of_members
+        i_price = inv_case.payment
+        i_vat_prec = inv_case.vat_percent
+        i_vat_included = inv_case.vat_includes
+        i_payer_reference = inv_case.payer_reference
+
+    except BillingCase.DoesNotExist:
+        logging.info("Billing case not found")
+
+
+    invoice_values = make_invoice.create_invoice(
+        payer_name=p_name,
+        payer_address=p_address,
+        payer_zip=p_zip,
+        payer_city=p_city,
+        payer_country=p_country,
+        payer_business_id=p_business_id,
+        service=i_service,
+        pcs=i_pcs,
+        price=i_price,
+        vat_perc=i_vat_prec,
+        vat_included=i_vat_included,
+        payer_reference=i_payer_reference
+    )   
+    invoicing_row = Invoice(
+        billing_case_id = inv_case,
+        billing_cust_id = billing_cust,
+        invoice_num = invoice_values['invoice_number'],
+        invoice_date = datetime.strptime(invoice_values['invoice_date'], "%d.%m.%Y").date(),
+        due_date = datetime.strptime(invoice_values['due_date'], "%d.%m.%Y").date(),
+        invoice_status = 'draft',
+        description = i_service,
+        salary_sum = None,
+        travel_exp_sum = None,
+        other_claims_sum = None,
+        amount_vat_0 = invoice_values['price'],
+        vat_percent = i_vat_prec,
+        vat_sum = invoice_values['vat_e'],  
+        total_amount = invoice_values['total_sum'],
+        reference = invoice_values['reference'],
+        bank_account = None,
+        paid_amount = 0,
+        penalty_interest = None,
+        payment_date = None,
+        payment_state = 'unpaid'
+    )
+    invoicing_row.save()
+
+
